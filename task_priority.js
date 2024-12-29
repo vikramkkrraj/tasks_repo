ea3a316a2a320d7fa966b20049aef5d5b0c6969b
Vikram <vikramkrraj9876@gmail.com>
2024-12-29 11:51:18 +0530

    task_priority

---
+++ task_priority.js
@@ -0,0 +1,11 @@
+const todo = [ "reading news paper", "attending lecture", "complete Assignment", "cooking" ,"having meal"];
+
+todo.shift();
+// console.log(todo);
+todo.unshift("have breakfast");
+todo.unshift("morning walk");
+// console.log(todo);
+todo[todo.length-1] = "listening music"
+console.log(todo);
+
+
