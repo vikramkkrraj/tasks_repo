5e3ab339db7e19cf70cfdf5e2af1384d017cccde
Vikram <vikramkrraj9876@gmail.com>
2024-12-29 11:26:01 +0530

    todo task

---
+++ todo.js
@@ -0,0 +1,6 @@
+const todo = [ "reading news paper", "attending lecture", "complete Assignment", "cooking" ,"having meal"];
+console.log(todo);
+todo.unshift("Morning walk");
+console.log(todo);
+todo.pop();
+console.log(todo);
