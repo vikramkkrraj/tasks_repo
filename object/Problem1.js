d2d52c8df206d64ea077db27da5c06de51749646
Vikram <vikramkrraj9876@gmail.com>
2025-01-05 01:42:39 +0530

    object problem1

---
+++ object/Problem1.js
@@ -0,0 +1,7 @@
+const book = { 
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien', 
+    year :  1937,
+}
+
+console.log(`title : ${book.title}  ${book.author} ${book.year}.`); // title : The Hobbit  J.R.R. Tolkien 1937.
